8584353a307da1fc4dcc1a13ad51362900c1cddc
Yauheni Paloika <john13ev@gmail.com>
2024-01-20 14:08:45 +0400

    feat: add 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,2 +1,12 @@
 const fs = require('node:fs');
-console.log(fs);
+const path = require('node:path');
+const { stdout } = process;
+
+const textFile = path.join(__dirname, 'text.txt');
+const stream = fs.createReadStream(textFile, 'utf-8');
+
+let data = '';
+
+stream.on('data', (chunk) => (data += chunk));
+stream.on('end', () => stdout.write(data));
+stream.on('error', (error) => console.log('Error', error.message));
